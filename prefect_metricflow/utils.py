9f24547205fa7d2d2a9b19451cd6bb60fbbb970d
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-07-24 11:34:23 +0200

    Added test with YAML config

--- prefect_metricflow/utils.py
+++ prefect_metricflow/utils.py
@@ -11,18 +11,32 @@ from prefect_metricflow.exceptions import MetricFlowFailureException
 
 def get_config_file_path(config_file_path: Optional[str] = None) -> str:
     """
-    TODO
+    Returns MetricFlow config file path.
+
+    Args:
+        config_file_path: The absolute path of MetricFlow config file.
+
+    Returns:
+        The absolute path of the configuration file.
+        The default path is returned if `config_file_path` is not provided.
     """
     return config_file_path or ConfigHandler().file_path
 
 
 def persist_config(config: Union[Dict, str], file_path: str) -> None:
     """
-    TODO
+    Persist the MetricFlow configuration on the file system.
+
+    Args:
+        config: Either a `dict` or a valid YAML string describing
+            the MetricFlow configuration.
+        file_path: Absolute path of the file where the configuration will be persisted.
     """
 
     if isinstance(config, dict):
         mf_config = config
+
+    # If the config is a string, try parsing as YAML
     elif isinstance(config, str):
         try:
             mf_config = safe_load(config)
@@ -30,5 +44,6 @@ def persist_config(config: Union[Dict, str], file_path: str) -> None:
             msg = f"Error while parsing provided MetricFlow config string: {e}"
             raise MetricFlowFailureException(msg)
 
+    # Persist the configuration on the file system
     with open(file_path, "w") as config_file:
         dump(mf_config, config_file)
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -12,7 +12,7 @@ from prefect_metricflow.tasks import materialize
 
 @mock.patch.dict(os.environ, {"MF_CONFIG_DIR": "/tmp/mf_config_dir"})
 @mock.patch("metricflow.api.metricflow_client.MetricFlowClient")
-def test_materialize_success(mf_client_mock):
+def test_materialize_success_with_config(mf_client_mock):
     class MetricFlowClientMock:
         def materialize(
             materialization_name: str,
@@ -49,6 +49,43 @@ def test_materialize_success(mf_client_mock):
 
 @mock.patch.dict(os.environ, {"MF_CONFIG_DIR": "/tmp/mf_config_dir"})
 @mock.patch("metricflow.api.metricflow_client.MetricFlowClient")
+def test_materialize_success_with_yaml_config(mf_client_mock):
+    class MetricFlowClientMock:
+        def materialize(
+            materialization_name: str,
+            start_time: Optional[str] = None,
+            end_time: Optional[str] = None,
+            config: Optional[Union[Dict, str]] = None,
+            config_file_path: Optional[str] = None,
+        ) -> SqlTable:
+
+            return SqlTable(db_name="foo", schema_name="foo", table_name="foo")
+
+    mf_client_mock.return_value = MetricFlowClientMock
+
+    @flow(name="test_flow_2")
+    def test_flow():
+        return materialize(
+            materialization_name="foo",
+            config="""
+            dwh_dialect: redshift
+            dwh_host: 'localhost'
+            dwh_port: 5439
+            dwh_user: 'foo'
+            dwh_password: 'foo'
+            dwh_database: 'db'
+            dwh_schema: 'foo'
+            model_path: 'foo'
+            """,
+        )
+
+    response = test_flow()
+
+    assert response == SqlTable(db_name="foo", schema_name="foo", table_name="foo")
+
+
+@mock.patch.dict(os.environ, {"MF_CONFIG_DIR": "/tmp/mf_config_dir"})
+@mock.patch("metricflow.api.metricflow_client.MetricFlowClient")
 def test_materialize_failure(mf_client_mock):
     class MetricFlowClientMock:
         def materialize(
@@ -62,9 +99,11 @@ def test_materialize_failure(mf_client_mock):
             return SqlTable(db_name="foo", schema_name="foo", table_name="foo")
 
     mf_client_mock.return_value = MetricFlowClientMock
-    mf_client_mock.side_effect = MetricFlowFailureException
 
-    @flow(name="test_flow_2")
+    msg = "Cannot build materialization!"
+    mf_client_mock.side_effect = MetricFlowFailureException(msg)
+
+    @flow(name="test_flow_3")
     def test_flow():
         return materialize(
             materialization_name="foo",
@@ -80,5 +119,5 @@ def test_materialize_failure(mf_client_mock):
             },
         )
 
-    with pytest.raises(MetricFlowFailureException):
+    with pytest.raises(MetricFlowFailureException, match=msg):
         test_flow()
